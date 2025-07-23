24b4831ce4481bd781be088bbddcfae6f54c4702
kanikashreesivakumar <kanikashreesivakumar16@gmail.com>
2025-07-23 22:58:27 +0530

    adeed some

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'ts-techy',
-  
+  icons: {
+    icon: '/images/ts-logo.webp', // or '/favicon.ico' if you have a favicon
+  },
 }
 
 export default function RootLayout({
